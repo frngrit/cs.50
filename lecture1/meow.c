22d62a4a5c480bcee6fc699f5764c245b282b924
frngrit <75774883+frngrit@users.noreply.github.com>
2022-06-25 09:30:13 +0000

    Sat, Jun 25, 2022, 9:30 AM +00:00

--- lecture1/meow.c
+++ lecture1/meow.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <cs50.h>
+
 
 //declare meow first
-void meow(int n);
+void meow(int n, string mes); //prototype
 
 
 
 int main(void)
 {
-    const int LIMIT = 3;
-    int i = 0;
 
-    while (i < LIMIT)
-    {
-        meow();
-        i += 1;
-    }
+    meow(3,"hi");
+
 
     /*
     for (int j = i; i > 0; i--)
@@ -26,7 +23,14 @@ int main(void)
 
 
 //If you define your function after execute it, there will be error
-void meow(void)
+void meow(int n, string mes)
 {
-    printf("meow~\n");
+    int i = 0;
+    while (i < n)
+    {
+        //print("meow\n"); old version print meow n times
+        printf("%s\n",mes); //new version print message from user n times
+        i += 1;
+    }
+
 }
