9f06e1d779822bf37cb00867b52a157a148365f8
frngrit <75774883+frngrit@users.noreply.github.com>
2022-06-25 09:39:50 +0000

    Sat, Jun 25, 2022, 9:39 AM +00:00

--- lecture1/discount.c
+++ lecture1/discount.c
@@ -1,10 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float discount(float reg_price, float sale_pct);
+
+
 int main(void)
 {
-    float regualar = get_float("Regular price: ");
-    float sale = regular * 0.85;
+    float regular = get_float("Regular price: ");
+    float sale_pct = get_float("Sale percentage: ");
+    float discounted = discount(regular,sale_pct);
+    //float sale = regular * 0.85;
 
-    printf(");
+    printf("Sale Price: %.2f\n",discounted);
+}
+
+float discount(float reg_price, float sale_pct)
+{
+    return reg_price * (1-sale_pct);
 }
