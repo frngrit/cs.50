b8c02d1b586fe53f0ea4080a7edd4b74b0e9ca07
frngrit <75774883+frngrit@users.noreply.github.com>
2022-06-27 03:50:10 +0000

    Mon, Jun 27, 2022, 3:50 AM +00:00

--- lect2/buggy2.c
+++ lect2/buggy2.c
@@ -6,7 +6,7 @@ int get_negative_int(void);
 int main(void)
 {
     int i = get_negative_int();
-    printf("number is %i\n",i);
+    printf("number is %i\n", i);
 }
 
 int get_negative_int(void)
@@ -14,8 +14,9 @@ int get_negative_int(void)
     int n;
     do
     {
+        //prompt user to give negative number
         n = get_int("Input negative number: ");
     }
-    while (n < 0); //Bug is in this line
+    while (n < 0); //Bug is in this line, should be while (n < 0);
     return n;
 }
