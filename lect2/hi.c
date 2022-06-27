9f3e4025c8781b82eff29e4f0ad291b47798ed56
frngrit <75774883+frngrit@users.noreply.github.com>
2022-06-27 08:33:46 +0000

    Mon, Jun 27, 2022, 8:33 AM +00:00

--- lect2/hi.c
+++ lect2/hi.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
 
-    printf("%i %i %i\n",c1,c2,c3);
+    // string c = "HI!";
+
+    int c[3];
+    c[0] = 1;
+    c[1] = 2;
+    c[2] = 3;
+
+    printf("%i %i %i %i\n",c[0],c[1],c[2],c[3]);
 }
