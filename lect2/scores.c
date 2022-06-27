562c6fc7a9259367b46931da8e5dc70432ab1d45
frngrit <75774883+frngrit@users.noreply.github.com>
2022-06-27 08:03:44 +0000

    Mon, Jun 27, 2022, 8:03 AM +00:00

--- lect2/scores.c
+++ lect2/scores.c
@@ -2,9 +2,19 @@
 
 int main(void)
 {
+    /*
     int score1 = 72;
     int score2 = 73;
     int score3 = 33;
+    */
 
-    printf("average score is %f",(score1 + score2 + score3)/3.0);
+   //use array instead
+   int scores[3];
+
+   scores[0] = 72;
+   scores[1] = 73;
+   scores[2] = 33;
+
+
+    printf("average score is %f\n",(scores[0] + scores[1] + scores[2])/3.0);
 }
