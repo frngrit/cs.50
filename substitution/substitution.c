d41c412901eb8fa0b3c0704442dc93c4b84da255
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 11:26:59 +0000

    Fri, Jul 1, 2022, 11:26 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -24,6 +24,7 @@ int main(int argc, string argv[])
         printf("Invalid characters in key.\n");
         return 1;
     }
+    
 
     string s = get_string("plaintext:  "); //prompt user for plain text
 
