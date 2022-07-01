a1fb3defe144034a8e707851f3246bef688a335e
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 09:28:21 +0000

    Fri, Jul 1, 2022, 9:28 AM +00:00

--- lect2/length.c
+++ lect2/length.c
@@ -1,16 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int string_length(name);
+
 int main(void)
 {
     string name = get_string("Name: ");
 
+    int length = strlen(name);
+
+    printf("%i\n", length);
+
+    // int i = 0;
+
+    // while (name[i] != '\0')
+    // {
+    //     printf("%c\n",name[i]);
+    //     i++;
+    // }
+    // printf("%c\n",name[i]);
+    // printf("%i\n",i);
+}
+
+int string_length(name)
+{
     int i = 0;
 
     while (name[i] != '\0')
     {
+        printf("%c\n", name[i]);
         i++;
     }
-
-    printf("%i\n",i);
+    return i;
 }
