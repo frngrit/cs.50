f15ba8775d80db5260c58e77d173c660c0134253
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 14:29:26 +0000

    Fri, Jul 1, 2022, 2:29 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) 
+int main(void)
 {
-
+    string s = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
+    printf("%lu\n",strlen(s));
 }
