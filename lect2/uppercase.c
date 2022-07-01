b1743555dfd599ee393f9296aa9a211eb8e6a113
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 10:00:24 +0000

    Fri, Jul 1, 2022, 10:00 AM +00:00

---
+++ lect2/uppercase.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before:  ");
+    printf("Output: ");
+
+    int b;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        b = (int) s[i];
+        if (b > 60)
+        {
+            b -= 32;
+        }
+        printf("%c",(char) b);
+    }
+    printf("\n");
+}
