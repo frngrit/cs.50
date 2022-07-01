e8c1308f34ceb530762847d802bd8601dec8d4b5
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 14:37:54 +0000

    Fri, Jul 1, 2022, 2:37 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,10 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
+#include <ctype.h>
 int main(void)
 {
     //prompt for user's string
     string s = get_string("Text: ");
-    printf("%i\n",(int) floor(2.53));
+    //count for letter
+    int i = 0, letters = 0, sentences = 0, words = ;
+    while (s[i] != '\0')
+    {
+        words += 1;
+        sentences += 1;
+        if (isalpha(s[i])
+        {
+            letters += 1;
+        }
+        if (s[i] == ' ')
+        {
+            words += 1;
+        }
+        if (
+    }
 }
