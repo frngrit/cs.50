b784c919c563378e908bb15fbfb4e15a011de824
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 14:32:42 +0000

    Fri, Jul 1, 2022, 2:32 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main(void)
 {
+    //prompt for 
     string s = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
-    printf("%lu\n",strlen(s));
+    printf("%i\n",(int) floor(2.53));
 }
