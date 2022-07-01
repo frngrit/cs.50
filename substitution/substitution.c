dbe6cb125582ea071b64b1479c5500f664cbd2b6
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 10:51:17 +0000

    Fri, Jul 1, 2022, 10:51 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+
+int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        //hadle when user input incorrectly
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    
+
 
 }
