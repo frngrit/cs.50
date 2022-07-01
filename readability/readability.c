29295c50e891f5a1a713394575ed0cf4a8263396
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 14:41:47 +0000

    Fri, Jul 1, 2022, 2:41 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,12 +8,11 @@ int main(void)
     //prompt for user's string
     string s = get_string("Text: ");
     //count for letter
-    int i = 0, letters = 0, sentences = 0, words = ;
+    int i = 0, letters = 0, sentences = 0, words = 0;
     while (s[i] != '\0')
     {
         words += 1;
-        sentences += 1;
-        if (isalpha(s[i])
+        if (isalpha(s[i]))
         {
             letters += 1;
         }
@@ -21,6 +20,13 @@ int main(void)
         {
             words += 1;
         }
-        if (
+        if (s[i] == ',' || s[i] == '.' || s[i] == '?' || s[i] == '!')
+        {
+            sentences += 1;
+        }
+
+        float index = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8;
+
+        printf("Grade %i", (int) round(index));
     }
 }
