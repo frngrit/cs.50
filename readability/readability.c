f4acab546acc3583804b909be58168abcf9ccb0f
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-01 15:00:10 +0000

    Fri, Jul 1, 2022, 3:00 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,5 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
 #include <ctype.h>
 int main(void)
@@ -14,24 +13,30 @@ int main(void)
 
         if (isalpha(s[i]))
         {
+            //increment in letter
             letters += 1;
         }
         if (s[i] == ' ')
         {
+            //increment in word
             words += 1;
         }
         if (s[i] == '.' || s[i] == '?' || s[i] == '!')
         {
+            //increment in sentence
             sentences += 1;
         }
         i++;
     }
     float index = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8;
     //printf("%i %i %i %f", letters, sentences, words, index);
+
+    //check if index more than or equal 16
     if (index >= 16)
     {
         printf("Grade 16+\n");
     }
+    //check if index less than or equal 1
     else if (index <= 1)
     {
         printf("Before Grade 1\n");
