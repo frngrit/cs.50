6c2cc723e306f259d52b3e2d038138fe053ffe4b
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-02 08:52:14 +0000

    Sat, Jul 2, 2022, 8:52 AM +00:00

--- try-sth/ternary.c
+++ try-sth/ternary.c
@@ -9,7 +9,7 @@ int main(void)
         int player1 = get_int("Enter attack damage: ");
         int player2 = get_int("Enter attack damage: ");
     }
-    while (isnumeric(player1) && isnumeric(player2))
+    while ( && isnumeric(player2))
     string answer = player1 > player 2 ? "Player1 wins\n" : player2 > player1 ? "Player2 wins\n" : "Tie\n";
 
     printf("%s\n", answer);
