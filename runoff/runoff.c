84d5e6f75f27587b35425a7d21e3bfc8211e4688
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 10:21:14 +0000

    Fri, Jul 8, 2022, 10:21 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,7 +143,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < voter
+    for (int i = 0; i < voter_count; i++)
+    {
+        
+    }
     // TODO
     return;
 }
