424c4794db2837c892fe0805b9a3d427fb769f7e
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 16:47:14 +0000

    Fri, Jul 8, 2022, 4:47 PM +00:00

--- try-sth/empty.c
+++ try-sth/empty.c
@@ -3,8 +3,8 @@
 int main(void)
 {
     int candidate_count = 4;
-    int pref[candidate_count];
-    int round = (candidate_count - 1) / 2 * candidate_count;
+    int round = (candidate_count - 1) * candidate_count / 2;
+    printf("%i\n", round);
     for (int i = 0; i < round; i++)
     {
         printf("at %i\n", i);
