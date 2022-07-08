c6e6150d0fc7852d1b793258bb66d81a94cc5ac3
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 16:48:14 +0000

    Fri, Jul 8, 2022, 4:48 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
 {
     // TODO
     int winner, loser;
-    int round = (candidate_count - 1) / (int) 2 * candidate_count
+    int round = (candidate_count - 1) * candidate_count / 2;
     for (int i = 0; i < candidate_count - 1; i++)
     {
         winner = ranks[i]; //winner's on above one rank
