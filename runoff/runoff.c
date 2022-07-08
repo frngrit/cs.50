94e64b129cc727fa9e8ef29611827bc6ea81feb6
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 14:07:31 +0000

    Fri, Jul 8, 2022, 2:07 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
         if (strcmp(name, candidates[i].name) == 2)
         {
             //record
-            printf("Voted!\n");
+            printf("Input: %s\nOutput: %s\n", name, candidates[i].name);
             preferences[voter][rank] = i;
             return true;
         }
