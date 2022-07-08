3bc6695b677e4c477c509ea3547fa3dfbd9e308d
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 20:04:49 +0000

    Fri, Jul 8, 2022, 8:04 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -245,7 +245,10 @@ void print_winner(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            
+            if (locked[i][j])
+            {
+                printf("");
+            }
         }
     }
     // TODO
