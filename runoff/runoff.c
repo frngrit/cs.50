90d748e8912dc78b1419618304e1865bc5647947
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 10:13:56 +0000

    Fri, Jul 8, 2022, 10:13 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name))
         {
-            
+            preferences[voter][rank]
         }
     }
 
