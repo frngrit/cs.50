a776107097ec30cb9e8d1a6214c219f688805715
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-08 10:11:26 +0000

    Fri, Jul 8, 2022, 10:11 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name))
+        {
+            
+        }
+    }
+
 
     // TODO
     return false;
