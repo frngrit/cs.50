3348260baf6d207be924c3477604f3ce683e78a3
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-09 06:48:02 +0000

    Sat, Jul 9, 2022, 6:48 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,8 +134,25 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    //loop over left-corner half of matrix
-    
+    //loop pref matrix
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for(int j = 0; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[j][i] > preferences[i][j])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+        }
+    }
     return;
 }
 
