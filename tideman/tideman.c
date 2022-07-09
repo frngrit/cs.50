6fb0a34760851b8391a675ac167a455180a48ebe
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-09 06:36:42 +0000

    Sat, Jul 9, 2022, 6:36 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -155,6 +155,13 @@ void add_pairs(void)
             pairs[n].winner = j;
             pairs[n].loser = i;
         }
+        else
+        {
+            pairs[n].winner = i;
+            pairs[n].loser = j;
+            pairs[n].winner = i;
+            pairs[n].winner = j;
+        }
         pair_count++;
         i++;
         if (j > candidate_count)
