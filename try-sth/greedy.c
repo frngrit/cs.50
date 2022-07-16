709d9bdf0248eeb68cb05b23a6678975b53622e5
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-16 04:13:13 +0000

    Sat, Jul 16, 2022, 4:13 AM +00:00

--- try-sth/greedy.c
+++ try-sth/greedy.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <math.h>
 
 int string_to_int(string input);
+int greedy_recur(int remain, int current);
 
 int main(int argc, string argv[])
 {
@@ -13,6 +15,8 @@ int main(int argc, string argv[])
     }
 
     string input = argv[1];
+    int numb = string_to_int(input);
+
 
 }
 
@@ -20,8 +24,20 @@ int main(int argc, string argv[])
 
 int string_to_int(string input)
 {
+    int output = 0;
     for(int i = 0, n = strlen(input); i < n; i++)
     {
-        
+        if (input[i] < 48 || input[i] > 57)
+        {
+            printf("Invalid input\n");
+            return 2;
+        }
+        output += pow(10,i) * (int) (input[i] - 48);
     }
+    return output;
+}
+
+int greedy_recur(int remain, int current)
+{
+    greedy_recur(int remain - 
 }
