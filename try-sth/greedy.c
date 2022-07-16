334f0567378e82700f6e62a91d58b1da10401460
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-16 04:06:23 +0000

    Sat, Jul 16, 2022, 4:06 AM +00:00

--- try-sth/greedy.c
+++ try-sth/greedy.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-int main(argc, argv[])
+int string_to_int(string input);
+
+int main(int argc, string argv[])
 {
     if(argc != 2)
     {
-        printf("Usage: greedy [number]");
+        printf("Usage: ./greedy [number]\n");
         return 1;
+    }
+
+    string input = argv[1];
+
+}
+
+
+
+int string_to_int(string input)
+{
+    for(int i = 0, n = strlen(input); i < n; i++)
+    {
+        
     }
 }
