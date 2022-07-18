aaa4ec769a1ccb5865768a16851e841c641d2b40
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-18 04:13:12 +0000

    Mon, Jul 18, 2022, 4:13 AM +00:00

--- lect4/adress-string.c
+++ lect4/adress-string.c
@@ -6,15 +6,6 @@ int main(void)
     string s = "Hi!";
     string *p = &s;
 
-    //printf("%c\n", *s);
-
-
-    for (int i = 0, char c = s[0]; c != '\0'; i++)
-    {
-        c = *(s + i);
-        printf("%c\n", c);
-    }
-
     for (int i = 0; s[i] != '\0'; i++)
     {
         char *b = &s[i];
