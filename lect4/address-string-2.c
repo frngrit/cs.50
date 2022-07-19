5b36935a11632bfd37a69efec7f3c795c2d79424
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-19 04:24:23 +0000

    Tue, Jul 19, 2022, 4:24 AM +00:00

---
+++ lect4/address-string-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string s = "Hi!";
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
+    printf("%c\n", s[3]);
+}
