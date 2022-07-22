fd20abb2709cc4ae56d8f3b1212d5177e426bda7
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-22 12:44:08 +0000

    Fri, Jul 22, 2022, 12:44 PM +00:00

---
+++ lect4/copy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
+
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    // char *t = s; //it's like you assign pointer to be address of address that stored in s
+
+    char *t = malloc(strlen(s) + 1);
+
+    for (int i = 0, n = strlen(s) + 1; i < n; i++)
+    {
+        t[i] = s[i];
+        // t[i] = *(s+i);
+    }
+    //or use this ------> strcpy(t, s);
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s @%p\n", s, s);
+    printf("t: %s @%p\n", t, t);
+
+}
