cb3c50194340a16d4b7328176f0748fb3cb7f8cf
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-22 13:19:28 +0000

    Fri, Jul 22, 2022, 1:19 PM +00:00

--- lect4/memory.c
+++ lect4/memory.c
@@ -5,12 +5,15 @@ int main(void)
 {
 
     int *x = malloc(3 * sizeof(int)); //return an array of three ints or address of first array of three ints
-    printf("%i\n", x[0]); //output is zero (such a fasinating!! ><)
+    // printf("%i\n", x[0]); //output is zero (such a fasinating!! ><)
     x[1] = 72;
     x[2] = 73;
     x[3] = 33;
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        printf("\n");
+        printf("%i\n", x[i+1]);
     }
+
+    free(x);
+    printf("%i\n", x[3]);
 }
