2b1c41a47017fad6a85547b9e529f86896acfac8
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-23 06:46:12 +0000

    Sat, Jul 23, 2022, 6:46 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -29,7 +29,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < (width / 2); j++)
         {
-            int buffer = 
+            int buffer = image[i][j];
         }
     }
     return;
