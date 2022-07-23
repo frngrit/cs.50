462160ebb264db15797373689cdd1e9f4ee0e27b
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-23 06:55:37 +0000

    Sat, Jul 23, 2022, 6:55 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -40,5 +40,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++) //control height (row)
+    {
+        for (int j = 0; j < width; j++) //control width (column)
+        {
+            copy[i][j] = image[i][j]; //make a copy byte
+        }
+    }
     return;
 }
