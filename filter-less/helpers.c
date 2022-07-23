6845326f9899fc3a9fa0c6afb5b56aa961d3f772
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-23 06:42:41 +0000

    Sat, Jul 23, 2022, 6:42 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++) //control height (row)
+    {
+        for (int j = 0; j < width; j++) //control width (column)
+        {
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3; //find average of 3 color
+            image[i][j].rgbtBlue = average; //set it all the same
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
