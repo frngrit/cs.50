4beb289647cb583b25b345983352e45e90fce980
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-23 06:44:18 +0000

    Sat, Jul 23, 2022, 6:44 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,6 +25,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            BYTE 
+        }
+    }
     return;
 }
 
