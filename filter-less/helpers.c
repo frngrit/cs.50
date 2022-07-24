14e3b34715346f8772e4ea93998eba30aa8b54ee
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-24 05:56:25 +0000

    Sun, Jul 24, 2022, 5:56 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++) // control width (column)
         {
-            float average = ceil((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0); // find average of 3 color
+            float average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0); // find average of 3 color
             image[i][j].rgbtBlue = (int) average;                                                         // set it all the same
             image[i][j].rgbtGreen = (int) average;
             image[i][j].rgbtRed = (int) average;
