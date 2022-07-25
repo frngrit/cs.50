2f26a9444f0fb93a119f10780f7578c7068a9f03
frngrit <75774883+frngrit@users.noreply.github.com>
2022-07-25 03:10:47 +0000

    Mon, Jul 25, 2022, 3:10 AM +00:00

--- try-sth/encap.c
+++ try-sth/encap.c
@@ -19,7 +19,8 @@ int main(void)
         {
             array[n].x = i;
             array[n].y = j;
-            printf("(%p, %p)\n", &array[n].x, &array[n].y);
+            printf("(%i, %i)\n", array[n].x, array[n].y);
+            printf("(%p, %p)\n", &(array[n].x), &(array[n].y));
         }
         n++;
     }
