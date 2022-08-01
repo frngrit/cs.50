e40aa4585ff6971452ea8697450f99b413d88a3f
frngrit <75774883+frngrit@users.noreply.github.com>
2022-08-01 04:45:41 +0000

    Mon, Aug 1, 2022, 4:45 AM +00:00

--- lect5/tree.c
+++ lect5/tree.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+void free_tree(node *root);
+void print_tree(node *root);
+
+int main(void)
+{
+    // initialize tree
+    node *tree = NULL;
+
+    // add the number to tree (first number)
+    node *n = malloc(sizeof(node));
+
+    //check if n is allocated memory
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 4;
+    n->left = NULL;
+    n->right = NULL;
+    //point tree to n
+    tree = n;
+
+    //Add second number to the three
+    n = malloc(sizeof(node));
+
+    //check if n is allocated memory
+    if (n == NULL)
+    {
+        free(tree);
+        return 1;
+    }
+
+    n->number = 4;
+    n->left = NULL;
+    n->right = NULL;
+
+
+}
