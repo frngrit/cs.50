160df0325bced08f28b1685672b57e34c8279184
frngrit <75774883+frngrit@users.noreply.github.com>
2022-08-04 05:51:49 +0000

    Thu, Aug 4, 2022, 5:51 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -102,6 +102,10 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
+    for (int i = 0; i < N; i++)
+    {
+        
+    }
     // TODO
     return false;
 }
