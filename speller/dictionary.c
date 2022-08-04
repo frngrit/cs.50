76e6f63e519edd4e7c17dd03cceeee388eb9d594
frngrit <75774883+frngrit@users.noreply.github.com>
2022-08-04 08:37:40 +0000

    Thu, Aug 4, 2022, 8:37 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -51,7 +51,11 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
 
-    return word[0] % N;
+    int total = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        
+    }
 
 
 }
