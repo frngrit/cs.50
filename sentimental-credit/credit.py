c49b565f50962dd715fa8c18fec45f561401a7a9
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-10 13:04:35 +0000

    Sat, Sep 10, 2022, 1:04 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,12 +4,35 @@
 # MTC 16-digit, start with 51, 52, 53, 54, 55
 # VIS 13-16 digit start with 4
 
-while True:
-    try:
-        credit_numb = int(input("Number: "))
-        break
-    except:
-        pass
+def get_int():
+    while True:
+        try:
+            credit_numb = int(input("Number: "))
+            return credit_numb
+        except:
+            pass
 
 # check if credit numb is valid with Luhn's algorithm
+def luhn_checking(credit_numb):
+    luhn_check = 0
+    flag = True
+    for s in str(credit_numb):
+        if flag:
+            luhn_check += int(s)
+            flag = False
+        else:
+            luhn_check += 2 * int(s)
 
+    str_luhn = str(luhn_check)
+    if str_luhn[-1] != '0':
+        return True
+    return False
+
+def main():
+    credit_numb = get_int()
+
+    if ~(luhn_checking(credit_numb)):
+        print('INVALID')
+        return
+
+main()
