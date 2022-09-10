6340a123058d18848c3c415cc78d57f2feac1c61
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-10 13:21:01 +0000

    Sat, Sep 10, 2022, 1:21 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -15,28 +15,21 @@ def get_int():
 # check if credit numb is valid with Luhn's algorithm
 def luhn_checking(credit_numb):
     luhn_check = 0
-    # flag = True
-    # for s in str(credit_numb):
-    #     if flag:
-    #         luhn_check += int(s)
-    #         flag = False
-    #     else:
-    #         luhn_check += 2 * int(s)
-    #         flag = True
-    # print(luhn_check)
+    flag = True
+    for s in str(credit_numb):
+        if flag:
+            luhn_check += int(s)
+            flag = False
+        else:
+            if int(s) > 5:
+                temp = int(s) * 2
+                luhn_check += temp % 10
+                luhn_check += temp // 10
+            else:
+                luhn_check += 2 * int(s)
+            flag = True
+        print(luhn_check)
 
-    first_chunk = str(credit_numb)[::2]
-    second_chunk = str(credit_numb)[1::2]
-    for s in first_chunk:
-        if int(s) > 5:
-            temp = int(s) * 2
-            luhn_check += temp % 10
-            luhn_check += temp // 10
-        luhn_check += int(s) * 2
-        print(s, luhn_check)
-
-    for s in second_chunk:
-        luhn_check += int(s)
 
 
 
