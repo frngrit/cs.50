d55d3bf32eaf711d0f0c77b29d670c7c2519772c
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-10 15:26:30 +0000

    Sat, Sep 10, 2022, 3:26 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,8 +3,31 @@ import re
 
 # prompt for user's string
 text = input("Text: ")
-# count for letter
-i = 0, letters = 0, sentences = 0, words = 1
 
-for i in text:
-    if (re.search("")
+# count for text length
+i = len(text)
+
+# count for letter in text
+all_letters = re.findall("[a-zA-z]", text)
+letters = len(all_letters)
+
+# count for sentences
+all_stoppers = re.findall("[.?!]",text)
+sentences = len(all_stoppers)
+
+# count for words
+all_words = text.split()
+words = 1 + len(all_words)
+
+# calculate for index
+index = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8
+index = round(index)
+
+# check if index more than or equal 16
+if index >= 16:
+    print("Grade 16+")
+# check if index less than or equal 1
+elif index <= 1:
+    print("Before Grade 1")
+else:
+    print(f"Grade {index}")
