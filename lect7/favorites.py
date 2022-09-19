1dda395a60c400704a7b9ee2fc1382f9afc94af3
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-19 08:16:49 +0000

    Mon, Sep 19, 2022, 8:16 AM +00:00

--- lect7/favorites.py
+++ lect7/favorites.py
@@ -1,21 +1,28 @@
 import csv
+import re
+
+
 titles = {}
 
+counter = 0
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        try:
-            titles[title] += 1
-        except KeyError:
-            titles[title] = 1
+        # try:
+        #     titles[title] += 1
+        # except KeyError:
+        #     titles[title] = 1
 
+        if re.search("^(OFFICE|THE.OFFICE)$", title):
+            counter += 1
 
+print(counter)
 # for title in sorted(titles):
 #     print(title)
 
-def get_value(title):
-    return titles[title]
+# def get_value(title):
+#     return titles[title]
 
 
 # for title in sorted(titles, key = lambda title: titles[title], reverse=True):
