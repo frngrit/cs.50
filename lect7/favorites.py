6034974a8e314cffa57cdc31a2124fd4a289373f
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-19 07:05:27 +0000

    Mon, Sep 19, 2022, 7:05 AM +00:00

--- lect7/favorites.py
+++ lect7/favorites.py
@@ -5,6 +5,14 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        titles[title] += 1
-for title in sorted(titles):
-    print(title)
+        try:
+            titles[title] += 1
+        except KeyError:
+            titles[title] = 1
+
+
+# for title in sorted(titles):
+#     print(title)
+
+for title in titles:
+    print(f"{title}: {titles[title]}")
