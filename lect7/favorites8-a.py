5055612dd9dd22c5b24c8905aa55fb7901fbc130
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-20 10:50:36 +0000

    Tue, Sep 20, 2022, 10:50 AM +00:00

--- lect7/favorites8-a.py
+++ lect7/favorites8-a.py
@@ -6,8 +6,8 @@ db = SQL("sqlite:///favorites.db")
 
 title = input("Title: ").strip()
 
-rows = db.execute("SELECT COUNT(*) AS counter FROM favorites WHERE title LIKE ?", title)
+rows = db.execute("SELECT title FROM favorites WHERE title LIKE ?", title)
 
-row = rows[0]
 
-print(row["counter"])
+for row in rows:
+    print(row["title"])
