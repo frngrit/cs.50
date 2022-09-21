8e392037bed6149a73d06cd774e5ad9803dca6dc
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-21 15:01:11 +0000

    Wed, Sep 21, 2022, 3:01 PM +00:00

--- fiftyville/note.sql
+++ fiftyville/note.sql
@@ -27,31 +27,31 @@ SELECT * FROM atm_transactions WHERE year = 2021 AND month = 8 AND day = 1 AND a
 +------+----------------+------+-------+-----+---------------+------------------+--------+
 |  id  | account_number | year | month | day | atm_location  | transaction_type | amount |
 +------+----------------+------+-------+-----+---------------+------------------+--------+
-| 1069 | 33637883       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
-| 1075 | 19531272       | 2021 | 8     | 1   | Carvalho Road | deposit          | 60     |
-| 1084 | 17171330       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
-| 1087 | 24064261       | 2021 | 8     | 1   | Carvalho Road | deposit          | 65     |
-| 1098 | 99031604       | 2021 | 8     | 1   | Carvalho Road | deposit          | 75     |
-| 1105 | 89843009       | 2021 | 8     | 1   | Carvalho Road | deposit          | 20     |
-| 1116 | 17161820       | 2021 | 8     | 1   | Carvalho Road | deposit          | 60     |
 | 1127 | 13156006       | 2021 | 8     | 1   | Carvalho Road | deposit          | 90     |
-| 1142 | 26191313       | 2021 | 8     | 1   | Carvalho Road | deposit          | 15     |
-| 1166 | 55322348       | 2021 | 8     | 1   | Carvalho Road | deposit          | 5      |
-| 1184 | 32179718       | 2021 | 8     | 1   | Carvalho Road | deposit          | 55     |
-| 1186 | 39774254       | 2021 | 8     | 1   | Carvalho Road | deposit          | 40     |
-| 1196 | 70504954       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
-| 1201 | 39696970       | 2021 | 8     | 1   | Carvalho Road | deposit          | 80     |
-| 1242 | 21149684       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
-| 1245 | 92647903       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
-| 1247 | 46222318       | 2021 | 8     | 1   | Carvalho Road | deposit          | 50     |
-| 1261 | 32134232       | 2021 | 8     | 1   | Carvalho Road | deposit          | 65     |
-| 1269 | 21149684       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
 | 1283 | 16194980       | 2021 | 8     | 1   | Carvalho Road | deposit          | 40     |
+| 1116 | 17161820       | 2021 | 8     | 1   | Carvalho Road | deposit          | 60     |
+| 1084 | 17171330       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
+| 1075 | 19531272       | 2021 | 8     | 1   | Carvalho Road | deposit          | 60     |
+| 1242 | 21149684       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
+| 1269 | 21149684       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
+| 1087 | 24064261       | 2021 | 8     | 1   | Carvalho Road | deposit          | 65     |
+| 1142 | 26191313       | 2021 | 8     | 1   | Carvalho Road | deposit          | 15     |
+| 1261 | 32134232       | 2021 | 8     | 1   | Carvalho Road | deposit          | 65     |
+| 1184 | 32179718       | 2021 | 8     | 1   | Carvalho Road | deposit          | 55     |
+| 1069 | 33637883       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
+| 1201 | 39696970       | 2021 | 8     | 1   | Carvalho Road | deposit          | 80     |
+| 1186 | 39774254       | 2021 | 8     | 1   | Carvalho Road | deposit          | 40     |
+| 1247 | 46222318       | 2021 | 8     | 1   | Carvalho Road | deposit          | 50     |
+| 1166 | 55322348       | 2021 | 8     | 1   | Carvalho Road | deposit          | 5      |
+| 1316 | 65190958       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
+| 1196 | 70504954       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
 | 1300 | 75571594       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
 | 1310 | 76849114       | 2021 | 8     | 1   | Carvalho Road | deposit          | 100    |
+| 1105 | 89843009       | 2021 | 8     | 1   | Carvalho Road | deposit          | 20     |
+| 1245 | 92647903       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
 | 1315 | 93622979       | 2021 | 8     | 1   | Carvalho Road | deposit          | 35     |
-| 1316 | 65190958       | 2021 | 8     | 1   | Carvalho Road | deposit          | 10     |
 | 1331 | 95773068       | 2021 | 8     | 1   | Carvalho Road | deposit          | 90     |
+| 1098 | 99031604       | 2021 | 8     | 1   | Carvalho Road | deposit          | 75     |
 +------+----------------+------+-------+-----+---------------+------------------+--------+
 
 
