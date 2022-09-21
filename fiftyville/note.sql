e9cc1cd48146c606caa5b78b5417e012d97d9073
frngrit <75774883+frngrit@users.noreply.github.com>
2022-09-21 16:27:12 +0000

    Wed, Sep 21, 2022, 4:27 PM +00:00

--- fiftyville/note.sql
+++ fiftyville/note.sql
@@ -90,4 +90,13 @@ AND transaction_type = "withdraw";
 +----------------+
 */
 
-SELECT 
+SELECT id FROM people
+WHERE license_plate IN(
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND hour = 10
+    AND minute BETWEEN 15 AND 25
+);
