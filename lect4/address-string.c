288bc2d51810db5b4ccf2468414bd3de71e8464d
frngrit <75774883+frngrit@users.noreply.github.com>
2023-09-21 09:39:52 +0000

    Thu, Sep 21, 2023, 9:39 AM +00:00

--- lect4/address-string.c
+++ lect4/address-string.c
@@ -5,12 +5,13 @@ int main(void)
 {
     // string s = "Hi!";
 
-    char *s = "Hi!";
+    // char *s = "Hi!";
+    char *s = {'H', 'i', '!'};
 
     for (int i = 0; s[i] != '\0'; i++)
     {
         char *b = &s[i];
-        printf("char: %c, address: %p\n",s[i], b);
+        printf("from %s -> char: %c, address: %p\n", s, s[i], b);
     }
 
     // printf("%p\n", p);
