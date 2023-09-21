1caf031e8656ca1acb874e22fba6075897d4a555
frngrit <75774883+frngrit@users.noreply.github.com>
2023-09-21 09:43:19 +0000

    Thu, Sep 21, 2023, 9:43 AM +00:00

--- lect4/address-string.c
+++ lect4/address-string.c
@@ -8,6 +8,11 @@ int main(void)
     // char *s = "Hi!";
     char s[] = {'H', 'i', '!'};
 
+    char *addressOfArray = s;
+    char *addressOfFirstEle = &s[0];
+
+    printf("addressOfArray = %p\naddressOfFirstEle = %p\n", addressOfArray, addressOfFirstEle);
+
     for (int i = 0; s[i] != '\0'; i++)
     {
         char *b = &s[i];
